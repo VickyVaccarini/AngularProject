051a12686d651d56c37e80e2a379c1307e5d1247
Vicky Vaccarini <vicky_vaccarini@hotmail.com>
2024-10-03 00:28:33 -0300

    Adelanto tp8

--- EmployeeCrudAngular/EmployeeCrudAngular/EmployeeCrudAngular/karma.conf.js
+++ EmployeeCrudAngular/EmployeeCrudAngular/EmployeeCrudAngular/karma.conf.js
@@ -5,13 +5,23 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-junit-reporter'),
+      require('karma-coverage'),  // Asegúrate de agregar esto
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    reporters: ['progress', 'junit'],
+    reporters: ['progress', 'junit', 'coverage'],  // Agregar 'coverage' aquí
     junitReporter: {
       outputDir: 'test-results',
       outputFile: 'test-results.xml',
       useBrowserName: false
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/'), // Especificar el directorio para la cobertura
+      subdir: '.',
+      reporters: [
+        { type: 'html' },      // Generar un informe HTML
+        { type: 'lcov' },      // Generar un informe LCOV
+        { type: 'text-summary' } // Resumen en texto en la consola
+      ],
     },
     port: 9876,
     colors: true,
